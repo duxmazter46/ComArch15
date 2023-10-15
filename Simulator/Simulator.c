6e3a74b7589de0151519bc7feccddcbdae0a1f83
PlubCPE <chakkapop_s@cmu.ac.th>
2023-10-16 01:43:17 +0700

    ีupdate simulator

--- Simulator/Simulator.c
+++ Simulator/Simulator.c
@@ -1,6 +1,8 @@
+#include<stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <unistd.h>
 
 /*
  Rules for Simulator 
@@ -15,7 +17,7 @@
 5) (simulator) Initialize ทุก registers ให้เป็น 0.
 */
 
-
+/* instruction-level simulator */
 #define NUMMEMORY 65536 /* maximum number of words in memory */
 #define NUMREGS 8 /* number of machine registers */
 #define MAXLINELENGTH 1000
@@ -27,20 +29,61 @@ typedef struct stateStruct {
     int numMemory;
 } stateType;
 
+
 void printState(stateType *);
+
+ int convertNum(int num)
+    {
+          /* convert a 16-bit number into a 32-bit integer */
+          if (num & (1<<15) ) {
+              num -= (1<<16);
+          }
+          return(num);
+    }
+
+// Function to convert a binary string to an integer
+int binaryStringToInt(const char* binary) {
+    return (int)strtol(binary, NULL, 2);
+}
+
+// Function to extract the opcode from a binary string
+int extractOpcode(const char* binaryValue) {
+    char opcodeBinary[4];
+    strncpy(opcodeBinary, binaryValue, 3);
+    opcodeBinary[3] = '\0';
+    return binaryStringToInt(opcodeBinary);
+}
+
+  void printState(stateType *statePtr)
+    {
+        int i;
+        printf("\n@@@\nstate:\n");
+        printf("\tpc %d\n", statePtr->pc);
+        printf("\tmemory:\n");
+        for (i=0; i<statePtr->numMemory; i++) {
+            printf("\t\tmem[ %d ] %d\n", i, statePtr->mem[i]);
+        }
+        printf("\tregisters:\n");
+        for (i=0; i<NUMREGS; i++) {
+            printf("\t\treg[ %d ] %d\n", i, statePtr->reg[i]);
+        }
+        printf("end state\n");
+    }   
+
 
 int main(int argc, char *argv[])
 {
     char line[MAXLINELENGTH];
     stateType state;
     FILE *filePtr;
+    int sum = 0;
 
     if (argc != 2) {
         printf("error: usage: %s <machine-code file>\n", argv[0]);
         exit(1);
     }
 
-    filePtr = fopen(argv[1], "r");
+    filePtr = fopen(argv[1], "r");       //fopen(argv[1], "r");
     if (filePtr == NULL) {
         printf("error: can't open file %s", argv[1]);
         perror("fopen");
@@ -60,28 +103,116 @@ int main(int argc, char *argv[])
             exit(1);
         }
         printf("memory[%d]=%d\n", state.numMemory, state.mem[state.numMemory]);
+         while (1) {
+        printState(&state);
+
+        int intValue = state.mem[state.pc];
+        char binaryValue[17];
+        itoa(intValue, binaryValue, 2);
+
+        char opcodeBinary[4];
+        strncpy(opcodeBinary, binaryValue, 3);
+        opcodeBinary[3] = '\0';
+        int opcode = binaryStringToInt(opcodeBinary);
+
+        int regA, regB, destReg, offsetField, memoryAddress;
+
+        switch (opcode) {
+            case 0: // add
+                regA = binaryStringToInt(&binaryValue[10]);
+                regB = binaryStringToInt(&binaryValue[13]);
+                destReg = binaryStringToInt(&binaryValue[29]);
+                state.reg[destReg] = state.reg[regA] + state.reg[regB];
+                state.pc++;
+                sum++;
+                break;
+
+            case 1: // nand
+                regA = binaryStringToInt(&binaryValue[10]);
+                regB = binaryStringToInt(&binaryValue[13]);
+                destReg = binaryStringToInt(&binaryValue[29]);
+                state.reg[destReg] = ~(state.reg[regA] & state.reg[regB]);
+                state.pc++;
+                sum++;
+                break;
+
+            case 2: // lw
+                regA = binaryStringToInt(&binaryValue[10]);
+                regB = binaryStringToInt(&binaryValue[13]);
+                offsetField = binaryStringToInt(&binaryValue[16]);
+                offsetField = convertNum(offsetField);
+                memoryAddress = state.reg[regA] + offsetField;
+                if (memoryAddress < 0 || memoryAddress >= NUMMEMORY) {
+                    printf("ERROR: Invalid memory address\n");
+                    exit(1);
+                }
+                state.reg[regB] = state.mem[memoryAddress];
+                state.pc++;
+                sum++;
+                break;
+
+            case 3: // sw
+                regA = binaryStringToInt(&binaryValue[10]);
+                regB = binaryStringToInt(&binaryValue[13]);
+                offsetField = binaryStringToInt(&binaryValue[16]);
+                offsetField = convertNum(offsetField);
+                memoryAddress = state.reg[regA] + offsetField;
+                if (memoryAddress < 0 || memoryAddress >= NUMMEMORY) {
+                    printf("ERROR: Invalid memory address\n");
+                    exit(1);
+                }
+                state.mem[memoryAddress] = state.reg[regB];
+                state.pc++;
+                sum++;
+                break;
+
+            case 4: // beq
+                regA = binaryStringToInt(&binaryValue[10]);
+                regB = binaryStringToInt(&binaryValue[13]);
+                offsetField = binaryStringToInt(&binaryValue[16]);
+                offsetField = convertNum(offsetField);
+                if (state.reg[regA] == state.reg[regB]) {
+                    sum++;
+                    state.pc = state.pc + 1 + offsetField;
+                } else {
+                    sum++;
+                    state.pc++;
+                }
+                break;
+
+            case 5: // jalr
+                regA = binaryStringToInt(&binaryValue[10]);
+                regB = binaryStringToInt(&binaryValue[13]);
+                state.reg[regB] = state.pc + 1;
+                if (regA == regB) {
+                    state.pc++;
+                } else {
+                    state.pc = state.reg[regA];
+                }
+                sum++;
+                break;
+
+            case 6: // halt
+                sum++;
+                printf("machine halted\n");
+                printf("total of %d instructions executed\n", sum);
+                printf("final state of machine:\n");
+                state.pc++;
+                printState(&state);
+                return 0;
+
+            case 7: // noop
+                sum++;
+                state.pc++;
+                break;
+
+            default:
+                printf("ERROR: Memory at location %d is illegible.\n", state.pc);
+                exit(1);
+        }
     }
-        printState(&state); // Call printState before instruction execution
-    
-        printState(&state); // Call printState before exiting
         return 0;
     }
 
-
-    
-    void printState(stateType *statePtr)
-    {
-        int i;
-        printf("\n@@@\nstate:\n");
-        printf("\tpc %d\n", statePtr->pc);
-        printf("\tmemory:\n");
-        for (i=0; i<statePtr->numMemory; i++) {
-            printf("\t\tmem[ %d ] %d\n", i, statePtr->mem[i]);
-        }
-        printf("\tregisters:\n");
-        for (i=0; i<NUMREGS; i++) {
-            printf("\t\treg[ %d ] %d\n", i, statePtr->reg[i]);
-        }
-        printf("end state\n");
-    }   
+} 
 
