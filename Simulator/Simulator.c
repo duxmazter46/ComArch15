7e1d5b0f9e09fd3b6bd38aa7d4eda164388a5b66
PlubCPE <chakkapop_s@cmu.ac.th>
2023-10-15 18:55:39 +0700

    Fix Simulator 2

--- Simulator/Simulator.c
+++ Simulator/Simulator.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 
 /*
  Rules for Simulator 
@@ -14,92 +15,54 @@
 5) (simulator) Initialize ทุก registers ให้เป็น 0.
 */
 
-// Define the maximum number of memory locations
-#define MAX_MEMORY 1024
 
-// Define the state structure
-struct stateStruct {
-    uint32_t registers[32];
-    uint32_t memory[MAX_MEMORY];
-    uint32_t numMemory;
-    uint32_t programCounter;
-};
+#define NUMMEMORY 65536 /* maximum number of words in memory */
+#define NUMREGS 8 /* number of machine registers */
+#define MAXLINELENGTH 1000
 
-// Function to initialize the state structure
-void initializeState(struct stateStruct *state) {
-    for (int i = 0; i < 32; i++) {
-        state->registers[i] = 0;
+typedef struct stateStruct {
+    int pc;
+    int mem[NUMMEMORY];
+    int reg[NUMREGS];
+    int numMemory;
+} stateType;
+
+void printState(stateType *);
+
+int main(int argc, char *argv[])
+{
+    char line[MAXLINELENGTH];
+    stateType state;
+    FILE *filePtr;
+
+    if (argc != 2) {
+        printf("error: usage: %s <machine-code file>\n", argv[0]);
+        exit(1);
     }
-    for (int i = 0; i < MAX_MEMORY; i++) {
-        state->memory[i] = 0;
+
+    filePtr = fopen(argv[1], "r");
+    if (filePtr == NULL) {
+        printf("error: can't open file %s", argv[1]);
+        perror("fopen");
+        exit(1);
     }
-    state->numMemory = 0;
-    state->programCounter = 0;
-}
 
-// Function to print the current state of the machine
-void printState(struct stateStruct *state) {
-    printf("@@@\nstate:\n");
-    printf("    pc %d\n", state->programCounter);
-    for (int i = 0; i < 32; i++) {
-        printf("    reg[%d] %d\n", i, state->registers[i]);
+    // Initialize registers to 0
+    for (int i = 0; i < NUMREGS; i++) {
+        state.reg[i] = 0;
     }
-    for (int i = 0; i < state->numMemory; i++) {
-        printf("    mem[%d] %d\n", i, state->memory[i]);
-    }
-}
 
-// Function to execute machine code
-void executeMachineCode(struct stateStruct *state) {
-    while (state->programCounter < state->numMemory) {
-        // Call printState before executing the command
-        printState(state);
-
-        // Fetch the instruction from memory
-        uint32_t instruction = state->memory[state->programCounter];
-        uint32_t opcode = (instruction >> 26) & 0x3F;
-        uint32_t rs = (instruction >> 21) & 0x1F;
-        uint32_t rt = (instruction >> 16) & 0x1F;
-        uint32_t rd = (instruction >> 11) & 0x1F;
-        int32_t immediate = instruction & 0xFFFF;
-
-        // Execute the instruction (simplified for illustration)
-        switch (opcode) {
-            case 0x20: // ADD
-                state->registers[rd] = state->registers[rs] + state->registers[rt];
-                break;
-            case 0x23: // LW
-                // Simplified memory access (assuming byte addressable memory)
-                state->registers[rt] = state->memory[state->registers[rs] + immediate];
-                break;
-            // Add cases for other instructions
-            // ...
-            default:
-                // Handle unknown instruction
-                // ...
-
+    for (state.numMemory = 0; fgets(line, MAXLINELENGTH, filePtr) != NULL;
+         state.numMemory++) {
+        if (sscanf(line, "%d", state.mem + state.numMemory) != 1) {
+            printf("error in reading address %d\n", state.numMemory);
+            exit(1);
         }
 
-        // Increment the program counter
-        state->programCounter++;
+        // Print the state before executing the command and exit
+        printState(&state);
+        exit(0);
     }
-
-    // Call printState before exiting
-    printState(state);
-}
-
-int main() {
-    struct stateStruct state;
-    initializeState(&state);
-
-    // Example: Load machine code into memory (replace this with actual machine code)
-    state.memory[0] = 0x20090001; // Example LW instruction
-    state.memory[1] = 0x0000000A; // Memory value at address 10
-
-    state.numMemory = 2;
-
-    // Execute machine code
-    executeMachineCode(&state);
 
     return 0;
 }
